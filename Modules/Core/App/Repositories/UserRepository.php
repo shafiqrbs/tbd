d01f4a6309b2b427f1dba6e719c3834dd53372f3
Shafiq Isalm <shafiqabs@gmail.com>
2024-02-17 23:22:27 +0600

    Update Location & User module.

--- Modules/Core/App/Repositories/UserRepository.php
+++ Modules/Core/App/Repositories/UserRepository.php
@@ -9,8 +9,30 @@
 namespace Modules\Core\App\Repositories;
 
 use Doctrine\ORM\EntityRepository;
+use Illuminate\Support\Facades\DB;
 
 class UserRepository extends EntityRepository {
+
+
+    public function listWithSearch(array $queryParams = [])
+    {
+        $entity = DB::table("users as e")
+            ->select(DB::raw("e.id as id,e.username as name"))->limit(5000)->get();
+        return $entity;
+    }
+
+
+    public function searchAutoComplete($q)
+    {
+        $query = $this->createQueryBuilder('e');
+        $query->select('e.id as id');
+        $query->addSelect('username.name) AS text');
+        $query->andWhere($query->expr()->like("e.username", "'$q%'"  ));
+        $query->orderBy('e.username', 'ASC');
+        $query->setMaxResults( '100' );
+        return $query->getQuery()->getResult();
+
+    }
 
 
 }
--- Modules/Core/routes/api.php
+++ Modules/Core/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\HeaderAuthenticationMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Modules\Core\App\Http\Controllers\CustomerController;
+use Modules\Core\App\Http\Controllers\LocationController;
+use Modules\Core\App\Http\Controllers\UserController;
 
 /*
     |--------------------------------------------------------------------------
@@ -20,6 +22,11 @@ use Modules\Core\App\Http\Controllers\CustomerController;
 Route::prefix('/customer')->middleware([HeaderAuthenticationMiddleware::class])->group(function() {
     Route::get('/select-search', [CustomerController::class,'selectSearch'])->name('customer_autosearch');
 });
-Route::apiResource('customer', 'CustomerController')->middleware([HeaderAuthenticationMiddleware::class]);
+Route::prefix('/location')->middleware([HeaderAuthenticationMiddleware::class])->group(function() {
+    Route::get('/select-search', [LocationController::class,'selectSearch'])->name('location_autosearch');
+});
+Route::apiResource('customer', CustomerController::class)->middleware([HeaderAuthenticationMiddleware::class]);
+Route::apiResource('location', LocationController::class)->middleware([HeaderAuthenticationMiddleware::class]);
+Route::apiResource('user', UserController::class)->middleware([HeaderAuthenticationMiddleware::class]);
 
 
