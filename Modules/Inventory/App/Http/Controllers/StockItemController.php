eed97c882df9bdfb259f2ddf6da8f8256c6c1170
Right_Brain_Raju <raju.rightbrainsolution@gmail.com>
2025-01-06 16:13:53 +0600

    sales price update & update ui not show issue

--- Modules/Inventory/App/Http/Controllers/StockItemController.php
+++ Modules/Inventory/App/Http/Controllers/StockItemController.php
@@ -220,15 +220,15 @@ class StockItemController extends Controller
             return $response;
         }
         // for price update
-        if ($request->get('field')=='price'){
-            $findStockItem->update(['price'=>$request->get('value')]);
+        if ($request->get('field')=='sales_price'){
+            $findStockItem->update(['sales_price'=>$request->get('value')]);
         }
         if ($request->get('field')=='purchase_price'){
             $findStockItem->update(['purchase_price'=>$request->get('value')]);
         }
 
         // for multi-price price update
-        if ($request->get('field')!='price' && $request->get('field')!='purchase_price'){
+        if ($request->get('field')!='sales_price' && $request->get('field')!='purchase_price'){
             $wholeSaleExists = StockItemPriceMatrixModel::where('stock_item_id',$id)
                 ->where('price_unit_id',$request->get('setting_id'))
                 ->where('product_id',$findStockItem->product_id)
