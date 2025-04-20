540e91d3ceec03f993234ef8a8e0113db9e27932
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-04-20 23:50:52 +0600

    Add discount module.

---
+++ Modules/Inventory/App/Entities/Discount.php
@@ -0,0 +1,284 @@
+<?php
+
+namespace Modules\Inventory\App\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Discount
+ *
+ * @ORM\Table( name = "inv_discount")
+ * @ORM\Entity()
+ */
+class Discount
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Config", cascade={"detach","merge"} )
+     * @ORM\JoinColumn(onDelete="CASCADE")
+     **/
+    private  $config;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=true)
+     */
+    private $description;
+
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="discount_percent",type="float" , nullable=true)
+     */
+    private $discountPercent;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="offer_quantity",type="float" , nullable=true)
+     */
+    private $offerQuantity;
+
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="discount_quantity",type="float" , nullable=true)
+     */
+    private $discountQuantity;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(type="boolean",options={"default"="false"})
+     */
+    private $isDiscount;
+
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(type="boolean",options={"default"="false"})
+     */
+    private $isRecurring;
+
+
+    /**
+     * @var string|null The mode of discount applied (e.g., Percent|Product).
+     *
+     * @ORM\Column(name="discount_mode", type="string", length=50, nullable=true)
+     */
+    private $discountMode;
+
+    /**
+     * @var string|null The discount required (e.g., Customer, Without Customer).
+     *
+     * @ORM\Column(name="discount_customer", type="string", length=50, nullable=true)
+     */
+    private $discountCustomer;
+
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(type="boolean" ,options={"default"="false"})
+     */
+    private $removeImage;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $path;
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="start_date", type="datetime")
+     */
+    private $startDate;
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="end_date", type="datetime")
+     */
+    private $endDate;
+
+
+    /**
+     * @Gedmo\Translatable
+     * @Gedmo\Slug(fields={"name"})
+     * @ORM\Column(length=255)
+     */
+    private $slug;
+
+    /**
+     * @Gedmo\Blameable(on="create")
+     * @ORM\ManyToOne(targetEntity="Modules\Core\App\Entities\User")
+     **/
+    private  $createdBy;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Modules\Core\App\Entities\User")
+     **/
+    private  $approvedBy;
+
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="status", type="boolean" )
+     */
+    private $status= true;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * @param mixed $config
+     */
+    public function setConfig($config)
+    {
+        $this->config = $config;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param bool $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+
+
+
+}
