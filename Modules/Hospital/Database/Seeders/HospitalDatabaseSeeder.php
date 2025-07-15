e0ded1f263ebdf6e14c83ae72d4fcb49e9977f30
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-07-15 14:52:53 +0600

    Create new module Hospital & medicine

---
+++ Modules/Hospital/Database/Seeders/HospitalDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Hospital\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class HospitalDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Medicine/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('medicine::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('medicine.name') !!}</p>
+@endsection
