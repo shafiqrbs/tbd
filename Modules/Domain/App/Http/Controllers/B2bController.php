4e041d2dbd1e7194adca4b70f7ef9bd3e5a0e891
rbraju3m <rbraju3m@gmail.com>
2025-10-12 22:40:53 +0600

    update

--- Modules/Domain/App/Http/Controllers/B2bController.php
+++ Modules/Domain/App/Http/Controllers/B2bController.php
@@ -569,7 +569,7 @@ class B2bController extends Controller
             'barcode' => $parentProduct->barcode,
             'alternative_name' => $parentProduct->alternative_name,
             'unit_id' => $parentProduct->unit_id,
-            'product_type_id' => $childProductType->id,
+            'product_type_id' => $childProductType?->id ?? null,
             'parent_id' => $parentProduct->id,
             'description' => $parentProduct->description,
             'vendor_id' => $findSubDomain->vendor_id,
