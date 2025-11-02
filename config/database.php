79463a6aee03344acd87511767081d8b5ed7d6d3
rbraju3m <rbraju3m@gmail.com>
2025-11-03 01:06:30 +0600

    set default timezone

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'modes'  => [
+            'modes' => [
                 'STRICT_TRANS_TABLES',
                 'NO_ENGINE_SUBSTITUTION',
                 'NO_ZERO_IN_DATE',
@@ -133,7 +133,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
