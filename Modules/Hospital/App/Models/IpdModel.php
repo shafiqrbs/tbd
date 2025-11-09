de4a19f8ce7e0573e1200a3870984959617f4873
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-11-09 23:23:18 +0600

    Where invoice uid implement.

--- Modules/Hospital/App/Models/IpdModel.php
+++ Modules/Hospital/App/Models/IpdModel.php
@@ -590,13 +590,15 @@ class IpdModel extends Model
     public static function patientChart($entity,$data)
     {
         $date = new \DateTime('now');
+        $vital_chart_json = (isset($data['vital_chart_json']) and json_encode($data['vital_chart_json']) ) ? json_encode($data['vital_chart_json']) : '';
+        $insulin_chart_json = (isset($data['insulin_chart_json']) and json_encode($data['insulin_chart_json'])) ? json_encode($data['vital_chart_json']) : '';
         AdmissionPatientModel::updateOrCreate(
             [
                 'hms_invoice_id'             => $entity,
             ],
             [
-                'vital_chart_json'       => json_encode($data['vital_chart_json']) ?? null,
-                'insulin_chart_json'       => json_encode($data['insulin_chart_json']) ?? null,
+                'vital_chart_json'       => $vital_chart_json,
+                'insulin_chart_json'       => $insulin_chart_json,
                 'updated_at'    => $date,
                 'created_at'    => $date,
             ]
