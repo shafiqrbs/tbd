34fdfe569a4c59879a278c37d4646d5dd0bc2a77
rbraju3m <rbraju3m@gmail.com>
2025-12-07 16:44:54 +0600

    cors issue

--- config/cors.php
+++ config/cors.php
@@ -1,29 +1,33 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://www.tbd.local',
         'https://health.poskeeper.com',
         'https://hms.tbhsd.gov.bd',
         'https://sandra.poskeeper.com',
-        'https://pos.poskeeper.com'
-
+        'https://pos.poskeeper.com',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
     ],
-
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type',
+        'X-Requested-With',
+        'Authorization',
+        'X-Api-Key',
+        'Accept',
+        'Origin',
+    ],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 3600,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false, // IMPORTANT for JWT
 ];
