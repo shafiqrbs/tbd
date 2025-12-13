91a21138e688da26f1791c2edca16becd1da9b44
rbraju3m <rbraju3m@gmail.com>
2025-12-14 02:16:36 +0600

    update

--- Modules/Hospital/App/Models/ReportModel.php
+++ Modules/Hospital/App/Models/ReportModel.php
@@ -693,10 +693,22 @@ class ReportModel extends Model
             ])
 
             ->groupBy('hms_invoice_particular.particular_id', 'hp.display_name')
-            ->havingRaw('SUM(CASE
-            WHEN hms_invoice_particular.status = 1
-             AND hit.created_at BETWEEN ? AND ?
-            THEN 1 ELSE 0 END) > 0', [$start, $end])
+            ->havingRaw('
+                (
+                    SUM(CASE
+                        WHEN hms_invoice_particular.status = 1
+                         AND hit.created_at BETWEEN ? AND ?
+                        THEN 1 ELSE 0 END) > 0
+                )
+                OR
+                (
+                    SUM(CASE
+                        WHEN hms_invoice_particular.is_refund = 1
+                         AND hms_invoice_particular.invoice_transaction_refund_id IS NOT NULL
+                         AND hitr.created_at BETWEEN ? AND ?
+                        THEN 1 ELSE 0 END) > 0
+                )
+            ', [$start, $end, $start, $end])
 
             ->orderBy('hp.display_name', 'ASC')
             ->get();
